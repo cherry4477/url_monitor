a9f5da9686b2b237a695bb6401d5b9c54b9568d1
wangfw <wangfw@wangfw.local>
2016-10-08 15:17:43 +0800

    dele

---
+++ template.sh
@@ -0,0 +1,15 @@
+metric="metricname"
+host=`hostname`
+ts=`date +%s`
+url="urlname"
+
+function send {
+    curl -X POST -d "[{\"metric\": \"$1\", \"endpoint\": \"$2\", \"timestamp\": $3,\"step\": 60,\"value\": $4,\"counterType\": \"GAUGE\",\"tags\": \"open\"}]" http://127.0.0.1:1988/v1/push
+}
+
+res=`curl -k  -L --connect-timeout 3 -w %{http_code} -o /dev/null -s $url`
+if [ $res -eq "000" ]; then
+    res=0
+fi
+echo $res
+send "url:"$metric  $host $ts $res
