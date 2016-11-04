5c06f5fa25c450eea4c5de0440e5a750797f2304
wfw2046 <wfw2046@126.com>
2016-11-04 08:33:55 +0000

    tset

---
+++ shell/60_souhu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+metric="souhu"
+host="of-url-monitior"
+ts=`date +%s`
+url="www.souhu.com"
+
+function send {
+    curl -X POST -d "[{\"metric\": \"$1\", \"endpoint\": \"$2\", \"timestamp\": $3,\"step\": 60,\"value\": $4,\"counterType\": \"GAUGE\",\"tags\": \"open\"}]" http://127.0.0.1:1988/v1/push
+}
+
+res=`curl -k  -L --connect-timeout 3 -w %{http_code} -o /dev/null -s $url`
+if [ $res -eq "000" ]; then
+    res=0
+fi
+echo $res
+send "url-monitor:"$metric  $host $ts $res
